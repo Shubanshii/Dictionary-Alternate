3e924ce6d2f7ed8891bf884eb66f118f8a62001c
Shubanshii <c_day@alumni.baylor.edu>
2017-04-26 15:38:58 +0700

    storing the words and defs

--- wordsapi2.js
+++ wordsapi2.js
@@ -1,5 +1,7 @@
 var WORDS_BASE_URL = 'https://wordsapiv1.p.mashape.com/words/';
 var resultLengthArray = [];
+var num = 0;
+var resultsArray=[];
 function getDataFromApi(searchTerm, callback) {
   var settings = {
     url: 'https://wordsapiv1.p.mashape.com/words/' + searchTerm + '',
@@ -18,15 +20,20 @@ function getDataFromApi(searchTerm, callback) {
 
 function storeResultLengths(data){
  // console.log(data, typeof data.results, data.results, data.results.length);
+  var query = $(".js-query").val().split(" ");
 
 resultLengthArray.push(data.results.length);
+resultsArray.push(data.results);
+num++;
+if(num === query.length){
+
+  updateDivs(data);
+
+}
 //console.log(resultLengthArray);
 }
 
-function displayResultLengths(){
 
- console.log(resultLengthArray);
-}
 
 function displayWORDSearchData(data) {
  // console.log(data.results.length);
@@ -44,7 +51,25 @@ function displayWORDSearchData(data) {
   
   $('.js-search-results').html(resultElement);
 }
+function updateDivs(data){
+
+  var query = $(".js-query").val().split(" ");
+  $('.js-search-results').append('<div class="word 1">' + query[0] + '</div>');
+   for(var x=0; x<resultsArray[0].length; x++){
+       $('.js-search-results').append('<div id="definition ' + (x+1) + 'class="word 1">' + resultsArray[0][x].definition + '</div>');
+
+   }
+  for(var i=1; i<query.length; i++){ 
+   $('.js-search-results').append('<div class="display-none" class="word' + (i+1) + '">' + query[i] + '</div>');
+   for(var x=0; x<resultsArray[i].length; x++){
+       $('.js-search-results').append('<div class="display-none" id="definition ' + (x+1) + 'class="word' + (i+1) + '">' + resultsArray[i][x].definition + '</div>');
+
+   }
+  }
+
+}
 function startApi(){
+
   var query = $(".js-query").val().split(" ");
     for(var i=0; i<query.length; i++){
 
@@ -52,7 +77,9 @@ function startApi(){
        // console.log(i);
         
       }
-      setTimeout(function(){ displayResultLengths(); }, 3000);
+
+     // setTimeout(function(){ displayResultLengths(); }, 2000);
+      $('h1, form, button').hide();
 }
 
 function watchSubmit() {
